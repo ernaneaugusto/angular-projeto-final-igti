f243aadd11999e693ca6ff5b93c9b038a3e900dd
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-01-19 16:12:59 -0300

    criado pagina Home e secao de pesquisa em main-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/layout/main-content/main-content.component.ts
+++ src/app/layout/main-content/main-content.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MainContentComponent implements OnInit {
 
   @Input() title: string = '';
+  @Input() isSearchSection: boolean = true;
 
   constructor() { }
 
