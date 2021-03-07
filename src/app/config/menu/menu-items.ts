77ce0351e93aa55eb4239da217c94fcf17179c36
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-03-07 17:13:25 -0300

    correcao icone menu

--- src/app/config/menu/menu-items.ts
+++ src/app/config/menu/menu-items.ts
@@ -1,6 +1,7 @@
 import { MenuItem } from "src/app/shared/models/menu-item/menu-item.interface";
 
 export const MENU: Array<MenuItem> = [
-    { description: 'Home', url: '/admin', isActive: true, icon: "fa fa-home" },
+    { description: 'Promoções', url: '/admin', isActive: true, icon: "fa fa-list" },
+    // { description: 'Home', url: '/admin', isActive: true, icon: "fa fa-home" },
     // { description: 'Promoções', url: '/' }
 ]
