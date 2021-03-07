0890aad88010a969c8257b9565379d1fcdff10ef
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-03-07 16:43:03 -0300

    layout pagina PromotionDetails

--- src/app/pages/new-promotion/new-promotion.module.ts
+++ src/app/pages/new-promotion/new-promotion.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { NewPromotionComponent } from './new-promotion.component';
 import { LayoutModule } from 'src/app/layout/layout.module';
-import { PromotionDetailsComponent } from './../promotion-details/promotion-details.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { PromotionDetailsModule } from './../promotion-details/promotion-details.module';
 
 const routes: Routes = [
   { path: '', component: NewPromotionComponent },
   { path: ':id', component: NewPromotionComponent },
-  { path: ':id/detalhes', component: PromotionDetailsComponent },
+  // { path: ':id/detalhes', component: PromotionDetailsComponent },  
+  {
+    path: ':id/detalhes',
+    loadChildren: () => import('./../promotion-details/promotion-details.module').then(promotionDetails => promotionDetails.PromotionDetailsModule)
+  }
 ]
 
 @NgModule({
@@ -20,6 +24,7 @@ const routes: Routes = [
 CommonModule,
   RouterModule.forChild(routes),
   LayoutModule,
+  PromotionDetailsModule,
   ReactiveFormsModule
   ],
   exports: [
--- src/app/pages/promotion-details/promotion-details.module.ts
+++ src/app/pages/promotion-details/promotion-details.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PromotionDetailsComponent } from './promotion-details.component';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutModule } from 'src/app/layout/layout.module';
 
 const routes: Routes = [
   { path: '', component: PromotionDetailsComponent },
@@ -11,6 +12,7 @@ const routes: Routes = [
   declarations: [PromotionDetailsComponent],
   imports: [
     CommonModule,
+    LayoutModule,
     RouterModule.forChild(routes)
   ],
   exports: [PromotionDetailsComponent]
