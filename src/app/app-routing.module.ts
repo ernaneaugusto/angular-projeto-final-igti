dc6a2575ada34f1eb8c238aa95bd8795025b90ea
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-03-07 15:24:33 -0300

    refactor navegacao para admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,23 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'admin',
     loadChildren: () => import('./pages/home/home.module').then(home => home.HomeModule)
   },
   {
-    path: 'login',
+    path: 'admin/login',
     loadChildren: () => import('./pages/login/login.module').then(login => login.LoginModule)
   },
   {
-    path: 'promocao',
+    path: 'admin/promocao',
     loadChildren: () => import('./pages/new-promotion/new-promotion.module').then(newPromotion => newPromotion.NewPromotionModule)
   },
   {
-    path: 'estabelecimento',
+    path: 'admin/estabelecimento',
     loadChildren: () => import('./pages/establishment/establishment.module').then(establishment => establishment.EstablishmentModule)
   },
   {
-    path: 'perfil',
+    path: 'admin/perfil',
     loadChildren: () => import('./pages/perfil/perfil.module').then(perfil => perfil.PerfilModule)
   }
 ]
--- src/app/config/menu/menu-items.ts
+++ src/app/config/menu/menu-items.ts
@@ -1,6 +1,6 @@
 import { MenuItem } from "src/app/shared/models/menu-item/menu-item.interface";
 
 export const MENU: Array<MenuItem> = [
-    { description: 'Home', url: '/', isActive: true, icon: "fa fa-home" },
+    { description: 'Home', url: '/admin', isActive: true, icon: "fa fa-home" },
     // { description: 'Promoções', url: '/' }
 ]
