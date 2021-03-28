d42bbb3232e55f70bd56bcfcab87f53d7f91d688
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-03-28 16:20:51 -0300

    funcao de edicao para Promocao e alteracao estilo input form

--- src/app/pages/new-promotion/new-promotion.component.ts
+++ src/app/pages/new-promotion/new-promotion.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NewPromotionService } from './services/new-promotion.service';
 import { Promotion } from './../../shared/models/promotion/promotion.interface';
+import { ActivatedRoute } from '@angular/router';
+import { PromotionModel } from 'src/app/shared/models/promotion/promotion.model';
 
 @Component({
   selector: 'app-new-promotion',
@@ -24,31 +26,105 @@ export class NewPromotionComponent implements OnInit {
     product: new FormControl('', [Validators.required]),
     weekDays: new FormControl('', [Validators.required]),
     description: new FormControl('', [Validators.required]),
+    establishmentId: new FormControl('', [Validators.required]),
   });
+
+  // Edit flow
+  public promotionId: string = '';
+  public isFlowEdit: boolean = false;
+  public promotion: Array<PromotionModel> = [];
 
   get f() {
     return this.form.controls;
   }
 
-  constructor(private newPromotionService: NewPromotionService) {}
+  constructor(
+    private newPromotionService: NewPromotionService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(() => {
+      const { id } = this.activatedRoute.snapshot.params;
+
+      if (id && id.length > 0) {
+        this.promotionId = id;
+        this.isFlowEdit = true;
+
+        this.newPromotionService
+          .getPromotionById(this.promotionId)
+          .subscribe((promotion: Promotion) => {
+            const {
+              name,
+              validity,
+              stars,
+              starsMin,
+              category,
+              product,
+              weekDays,
+              description,
+              establishmentId,
+              expirate,
+            } = promotion;
+
+            this.form.setValue({
+              name,
+              validity,
+              stars,
+              starsMin,
+              category,
+              product,
+              weekDays,
+              description,
+              establishmentId,
+              expirate,
+            });
+          });
+      }
+    });
+  }
 
   public submitForm(): void {
     const data: Promotion = this.form.value;
-    data.establishmentId = '1'; 
+    data.establishmentId = '1';
 
-    this.newPromotionService.setNewPromotion(this.form.value).subscribe(
-      () => {
-        this.form.reset();
-        this.submitInfo.message = "Promoção cadastrada com sucesso!";
-        this.submitInfo.type = "success";
-      },
-      () => {
-        this.errorSubmit = true;
-        this.submitInfo.message = "Erro ao cadastrar Promoção!";
-        this.submitInfo.type = "danger";
-      }
-    );
+    // Reset error infos
+    this.submitInfo = {
+      message: '',
+      type: '',
+    };
+
+    if (!this.isFlowEdit) {
+      this.newPromotionService.setNewPromotion(this.form.value).subscribe(
+        () => {
+          this.form.reset();
+          this.submitInfo.message = 'Promoção cadastrada com sucesso!';
+          this.submitInfo.type = 'success';
+        },
+        () => {
+          this.errorSubmit = true;
+          this.submitInfo.message = 'Erro ao cadastrar Promoção!';
+          this.submitInfo.type = 'danger';
+        }
+      );
+    } else {
+      const editData = {
+        ...this.form.value,
+        id: this.promotionId,
+      };
+
+      this.newPromotionService.editPromotion(editData).subscribe(
+        () => {
+          this.form.reset();
+          this.submitInfo.message = 'Promoção atualizada com sucesso!';
+          this.submitInfo.type = 'success';
+        },
+        () => {
+          this.errorSubmit = true;
+          this.submitInfo.message = 'Erro ao atualizar Promoção!';
+          this.submitInfo.type = 'danger';
+        }
+      );
+    }
   }
 }
