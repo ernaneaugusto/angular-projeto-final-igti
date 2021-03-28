24390add906db266d03e30ff838cb32dddc2a722
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-03-28 16:18:38 -0300

    correcao estilo form e atualizado PromotionModel

--- src/app/shared/models/promotion/promotion.model.ts
+++ src/app/shared/models/promotion/promotion.model.ts
@@ -5,10 +5,28 @@ export class PromotionModel {
   public id: string;
   public name: string;
   public description: string;
+  public validity: string;
+  public stars: number;
+  public starsMin: number;
+  public category: string;
+  public expirate: string;
+  public product: string;
+  public weekDays: string;
+  public establishmentId: string;
+  public isFavorite?: boolean;
 
   constructor(data: Promotion) {
     this.id = data.id;
     this.name = data.name;
     this.description = data.description;
+    this.validity = data.validity;
+    this.stars = data.stars;
+    this.starsMin = data.starsMin;
+    this.category = data.category;
+    this.expirate = data.expirate;
+    this.product = data.product;
+    this.weekDays = data.weekDays;
+    this.establishmentId = data.establishmentId;
+    this.isFavorite = data.isFavorite;
   }
 }
