811ac11e3421711a281dfca60da5d4643342c731
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-04-11 19:27:50 -0300

    criado LoginGuard

---
+++ src/app/guards/login.guard.ts
@@ -0,0 +1,29 @@
+import { Route } from '@angular/compiler/src/core';
+import { Injectable } from '@angular/core';
+// import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { UrlTree, CanLoad } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from './../services/login.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanLoad {
+  constructor(private loginService: LoginService) {}
+
+  canLoad(
+    route: Route
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const user = this.loginService.getUserLocalStorage('user');
+    let isLogin = user.length != 0 ? true : false;
+
+    if (!isLogin) {
+      this.loginService.clearLocalStorage();
+    }
+    return isLogin;
+  }
+}
