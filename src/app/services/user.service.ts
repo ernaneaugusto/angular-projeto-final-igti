337c52017ffa8ff8bfa3c86adaaae62ed151b21d
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-04-11 17:48:15 -0300

    add service de busca por usuario e senha

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { URL } from './../config/urls';
 import { User } from './../shared/models/user/user.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -20,5 +21,9 @@ export class UserService {
 
   public getUserById(id: string) {
     return this.http.get<User>(`${URL.baseUrl}/${URL.users}/${id}`);
+  }
+  
+  public getUserByEmailAndPassword(email: string, password: string): Observable<Array<User>> {
+    return this.http.get<Array<User>>(`${URL.baseUrl}/${URL.users}?email=${email}&password=${password}`);
   }
 }
