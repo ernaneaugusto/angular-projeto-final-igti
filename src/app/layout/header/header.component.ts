9a336f7cfe267878d37f950f3da93654b4571dce
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-04-11 20:50:04 -0300

    regra para esconder botao do header quando nao logado

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from './../../services/login.service';
+import { ActivatedRoute, Router, RouterEvent, Event, NavigationStart } from '@angular/router';
+import { Location } from "@angular/common";
 
 @Component({
   selector: 'app-header',
@@ -7,11 +9,22 @@ import { LoginService } from './../../services/login.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public isLogged = false;
 
-  constructor(private loginService: LoginService) { }
-
-  ngOnInit(): void {
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+  ) {
+    this.router
+      .events
+      .subscribe((event: Event) => {
+        if(event instanceof NavigationStart && event.url !== '/' && event.url !== '/login') {
+          this.isLogged = true;
+        }
+      });
   }
+
+  ngOnInit(): void { }
 
   public logout(): void {
     this.loginService.logout();
