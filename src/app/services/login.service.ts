bf978cecef74c8a888cb50dbae7be2efed663ad4
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-04-12 01:56:23 -0300

    refactor LoginService para add establishmentId

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { User, UserLocalData } from './../shared/models/user/user.interface';
+import { UserLocalData } from './../shared/models/user/user.interface';
 import { UserService } from './user.service';
 import { Router } from '@angular/router';
+import { EstablishmentService } from './establishment.service';
+import { forkJoin } from 'rxjs';
 
 const USER_KEY = 'user';
 
@@ -16,46 +18,61 @@ export class LoginService {
 
   constructor(
     private userService: UserService,
-    private router: Router,
+    private establishmentService: EstablishmentService,
+    private router: Router
   ) {}
 
   public login(email: string, password: string): void {
-    this.userService
-        .getUserByEmailAndPassword(email, password)
-        .subscribe((users: Array<User>) => {
-          // Verifica se existe o usuario e senha informados
-          const user = users.filter(
-            (user) => user.email === email && user.password === password
-          );
+    forkJoin([
+      this.userService.getUserByEmailAndPassword(email, password),
+      this.establishmentService.getEstablishments(),
+    ]).subscribe(([userResponse, estabResponse]) => {
+      const userData: UserLocalData = {
+        id: '',
+        type: '',
+      };
 
-          if (user.length > 0) {
-            const { id, type } = user[0];
-            this.setUserLocalStorage({ id, type });
-            
-            // @TODO: corrigir rotas admin e cliente
-            // const url = type === "E" ? "/admin" : "";
-            const url = "/admin";
-            
-            this.router.navigateByUrl(url);
-          } else {
-            alert("Usuário ou senha incorretos!");
-          }
-        });
+      // Verifica se existe o usuario e senha informados
+      const user = userResponse.filter(
+        (user) => user.email === email && user.password === password
+      );
+
+      if (user.length > 0) {
+        const { id, type } = user[0];
+
+        userData.id = id;
+        userData.type = type;
+
+        if (type === 'E') {
+          userData.establishmentId = `${estabResponse[0].establishmentId}`;
+        }
+
+        this.setUserLocalStorage(userData);
+
+        // @TODO: corrigir rotas admin e cliente
+        // const url = type === "E" ? "/admin" : "";
+        const url = '/admin';
+
+        this.router.navigateByUrl(url);
+      } else {
+        alert('Usuário ou senha incorretos!');
+      }
+    });
   }
-  
-  public logout(): void {
-    const confirmLogout = confirm("Tem certeza que deseja sair?");
 
-    if(confirmLogout) {
+  public logout(): void {
+    const confirmLogout = confirm('Tem certeza que deseja sair?');
+
+    if (confirmLogout) {
       localStorage.clear();
-      this.router.navigateByUrl("/login");
+      this.router.navigateByUrl('/login');
     }
   }
- 
+
   public clearLocalStorage(): void {
-    alert("Você precisa estar logado para acessar essa página!");
+    alert('Você precisa estar logado para acessar essa página!');
     localStorage.clear();
-    this.router.navigateByUrl("/login");
+    this.router.navigateByUrl('/login');
   }
 
   public setUserLocalStorage(user: UserLocalData): void {
@@ -70,15 +87,17 @@ export class LoginService {
   }
 
   private formatUserLocalStorage(data: string): UserLocalData {
-    const localData = {
+    const localData: UserLocalData = {
       id: '',
       type: '',
+      establishmentId: '',
     };
 
     if (data.length !== 0) {
       const parseData = JSON.parse(data);
       localData.id = parseData.id;
       localData.type = parseData.type;
+      localData.establishmentId = parseData.establishmentId ? parseData.establishmentId : '';
     }
     return localData;
   }
