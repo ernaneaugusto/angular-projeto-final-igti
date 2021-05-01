9f859ba7f1002b7e69373ee006c42cc94fa35f27
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-05-01 17:10:29 -0300

    atualizacao interface User

--- src/app/shared/models/user/user.interface.ts
+++ src/app/shared/models/user/user.interface.ts
@@ -2,13 +2,17 @@ import { Promotion } from './../promotion/promotion.interface';
 
 export interface User {
   id: string;
-  birthday?: string;
-  password?: string;
-  confirmPassword?: string;
   promotions?: Array<Promotion>;
   name: string;
   email: string;
   type: string;
+  birthday?: string;
+  password?: string;
+  confirmPassword?: string;
+  address?: string;
+  city?: string;
+  district?: string;
+  state?: string;
 }
 
 export interface UserLocalData {
