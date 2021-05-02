f14fb240b535a8d59ef9ed6e927f3030f37129eb
Ernane Augusto <ernaneatcipriano@gmail.com>
2021-05-02 19:07:34 -0300

    separacao comportamento Home para Cliente e Estabelecimento

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,6 +5,8 @@ import { PromotionModel } from './../../shared/models/promotion/promotion.model'
 import { Subscription } from 'rxjs';
 import { LoginService } from './../../services/login.service';
 import { StatusMessage } from './../../shared/components/status-message/model/status-message.interface';
+import { UserService } from 'src/app/services/user.service';
+import { PromotionInfo } from './../../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +14,11 @@ import { StatusMessage } from './../../shared/components/status-message/model/st
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  public promotions: Array<PromotionModel> = [];
+  // Promocoes para Cliente
+  public allPromotionsClient: Array<PromotionModel> = [];
+  public promotionsClient: Array<PromotionInfo> = [];
+  // Promocoes para Admin
+  public promotionsAdmin: Array<PromotionModel> = [];
   public favorites: Array<PromotionModel> = [];
   public isAdmin: boolean = false;
   public yourPromotionsInfo: StatusMessage = {
@@ -21,41 +27,86 @@ export class HomeComponent implements OnInit, OnDestroy {
     show: false,
   };
   private subs$: Subscription = new Subscription();
+  private promotionInfo: PromotionInfo = {
+    id: '',
+    userId: '',
+    promotionId: '',
+    promotionName: '',
+    promotionDescription: '',
+    isFavorite: false,
+  };
 
   constructor(
     private promotionsService: PromotionsService,
+    private userService: UserService,
     private loginService: LoginService,
   ) {}
 
   ngOnInit(): void {
     const {id, type} = this.loginService.getUserLocalStorage();
+    this.promotionInfo.id = id;
 
     this.isAdmin = type === 'E' ? true : false;
     
-    this.subs$.add(
-      this.promotionsService
-        .getPromotionsByEstablishment(id)
-        .subscribe((data: Array<Promotion>) => {
-          const { promotions, favorites } = this.filterPromotions(data);
-          this.promotions = this.createModel(promotions);
-          this.favorites = this.createModel(favorites);
+    if(type === 'E') {
+      this.subs$.add(
+        this.promotionsService
+          .getPromotionsByEstablishment(id)
+          .subscribe((data: Array<Promotion>) => {
+            const { promotions, favorites } = this.filterPromotions(data);
+            this.promotionsAdmin = this.createModel(promotions);
+            this.favorites = this.createModel(favorites);
+            console.log("## promo admin", this.promotionsAdmin);
 
-          if(this.promotions.length === 0) {
-            this.yourPromotionsInfo.message = "Você não possui nenhuma Promoção!";
-            this.yourPromotionsInfo.type = "warning";
+            if(this.promotionsAdmin.length === 0) {
+              this.yourPromotionsInfo.message = "Você não possui nenhuma Promoção!";
+              this.yourPromotionsInfo.type = "warning";
+              this.yourPromotionsInfo.show = true;
+            }
+          },
+          () => {
+            this.yourPromotionsInfo.message = "Ocorreu algum erro. Tente novamente.";
+            this.yourPromotionsInfo.type = "danger";
             this.yourPromotionsInfo.show = true;
-          }
-        },
-        () => {
-          this.yourPromotionsInfo.message = "Ocorreu algum erro. Tente novamente.";
-          this.yourPromotionsInfo.type = "danger";
-          this.yourPromotionsInfo.show = true;
-        })
-    );
-  }
+          })
+      );
 
+      return;
+    }
+
+    this.getAllPromotions();
+    this.getClientPromotions();
+  }
+  
   ngOnDestroy(): void {
     this.subs$.unsubscribe();
+  }
+
+  public getClientPromotions() {
+    this.userService
+      .getPromotionByUser(this.promotionInfo.id)
+      .subscribe((promos: Array<PromotionInfo>) => {
+        this.promotionsClient = this.createPromotionInfoModel(promos);
+        console.log("##promos ", promos);
+      },
+      () => {
+        this.yourPromotionsInfo.message = "Ocorreu algum erro ao buscar suas Promoções. Tente novamente.";
+        this.yourPromotionsInfo.type = "danger";
+        this.yourPromotionsInfo.show = true;
+      });
+  }
+
+  public getAllPromotions() {
+    this.promotionsService
+      .getPromotions()
+      .subscribe((promos: Array<PromotionModel>) => {
+        this.allPromotionsClient = this.createModel(promos);
+      },
+      () => {
+        this.yourPromotionsInfo.message = "Ocorreu algum erro ao buscar as Promoções. Tente novamente.";
+          this.yourPromotionsInfo.type = "danger";
+          this.yourPromotionsInfo.show = true;
+      });
   }
 
   public deletePromotion(event: PromotionModel) {
@@ -92,6 +143,16 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     promotion.map((promo: Promotion) => {
       promotionModel.push(new PromotionModel(promo));
+    });
+
+    return promotionModel;
+  }
+  
+  private createPromotionInfoModel(promotion: Array<PromotionInfo>) {
+    const promotionModel: Array<PromotionInfo> = [];
+
+    promotion.map((promo: PromotionInfo) => {
+      promotionModel.push(promo);
     });
 
     return promotionModel;
